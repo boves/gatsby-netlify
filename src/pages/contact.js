e33156e25be406423e2bdb66a0e758d4348b7387
Daiv Boveri <daiv@metamesh.com>
2023-01-26 17:05:10 -0600

    Task: setting up site

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const ContactPage = () => {
+  return (
+    <div>
+        <h1>Hey, everybody!</h1>
+        <p>This is my new site using Gatsby and Netlify.</p>
+        <p>Soon there will be a contact form here.</p>
+        <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default ContactPage 
